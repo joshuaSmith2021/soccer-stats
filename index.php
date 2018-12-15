9b65dc811f28a3892fcf185cdf98d7d2fa8799c1
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 07:43:53 -0800

    dev

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $request_body = file_get_contents('php://input');
-} else {
-  echo 'Method not allowed.';
-}
-?>
---
+++ update-stats.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $approved_keys = [
+    '6f070951-0da6-4349-ac6f-4b305875a6ab'
+  ];
+  $request_body = file_get_contents('php://input');
+  $json = json_decode($request_body);
+  $data_file = fopen('player_stats.json', 'w') or die('Unable to open file');
+} else {
+  echo 'Method not allowed.';
+}
+?>
