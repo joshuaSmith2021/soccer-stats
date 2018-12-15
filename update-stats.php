0695072b238d7f669c7b3c6558c14e6fc448982f
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 08:21:29 -0800

    :joy:

--- update-stats.php
+++ update-stats.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     '6f070951-0da6-4349-ac6f-4b305875a6ab'
   ];
   $request_body = file_get_contents('php://input');
-  $new_data = substr(utf8_decode(urldecode($request_body)));
+  $new_data = substr(utf8_decode(urldecode($request_body)), 9);
   $data_file = fopen('player_stats.json', 'w') or die('Unable to open file');
   fwrite($data_file, $new_data);
   fclose($data_file);
