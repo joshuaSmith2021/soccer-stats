72d0a84613012c4d689575b733d1bca8d0495dfb
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 08:05:32 -0800

    update update stats phph

--- update-stats.php
+++ update-stats.php
@@ -4,11 +4,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     '6f070951-0da6-4349-ac6f-4b305875a6ab'
   ];
   $request_body = file_get_contents('php://input');
-  $json = json_decode($request_body);
   $data_file = fopen('player_stats.json', 'w') or die('Unable to open file');
   fwrite($data_file, $json->new_data);
   fclose($data_file);
-  echo $json;
+  echo $request_body;
 } else {
   echo 'Method not allowed.';
 }
