ac29612b2d165221875e314c233dd573a033de15
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 08:33:26 -0800

    add key check

--- update-stats.php
+++ update-stats.php
@@ -3,12 +3,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $approved_keys = [
     '6f070951-0da6-4349-ac6f-4b305875a6ab'
   ];
-  $request_body = file_get_contents('php://input');
-  $new_data = substr(utf8_decode(urldecode($request_body)), 9);
-  $data_file = fopen('player_stats.json', 'w') or die('Unable to open file');
-  fwrite($data_file, $new_data) or die('Unable to write to file');
-  fclose($data_file);
-  echo $new_data;
+  if (in_array($_GET['key'], $approved_keys)) {
+    $request_body = file_get_contents('php://input');
+    $new_data = substr(utf8_decode(urldecode($request_body)), 9);
+    $data_file = fopen('player_stats.json', 'w') or die('Unable to open file');
+    fwrite($data_file, $new_data) or die('Unable to write to file');
+    fclose($data_file);
+    echo $new_data;
+  } else {
+    echo 'Invalid key';
+  }
 } else {
   echo 'Method not allowed.';
 }
