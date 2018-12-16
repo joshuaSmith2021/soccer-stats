4736768cd2f895e475d88a7f356d9f0959d0bdae
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 18:40:37 -0800

    Update update_stats.py

--- update_stats.py
+++ update_stats.py
@@ -69,14 +69,15 @@ if __name__ == '__main__':
     old_data = json.loads(requests.get('https://stat-display.herokuapp.com/player_stats.json').text)
     player_count = old_data['player_count']
     games_played = old_data['games_played']
-
     player_data = get_player_data(service)
-
-    update_request = requests.post('https://stat-display.herokuapp.com/update-stats.php?key=6f070951-0da6-4349-ac6f-4b305875a6ab', data={
+    
+    payload = {
         'player_data': player_data,
         'player_count': player_count,
         'games_played': games_played
-    })
+    }
+
+    update_request = requests.post('https://stat-display.herokuapp.com/update-stats.php?key=6f070951-0da6-4349-ac6f-4b305875a6ab', data=json.dumps(payload))
     print(player_data)
     print(update_request.status_code)
     print(update_request.text)
