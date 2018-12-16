af8539222edccbf84900395e6e3f437080631357
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 19:05:47 -0800

    Update update_players.py

--- update_players.py
+++ update_players.py
@@ -1,13 +1,13 @@
-import json
-import requests
+from json import dumps, loads
+from requests import post
 
-data = json.loads(requests.get('https://stat-display.herokuapp.com/player_stats.json').text)
+data = loads(requests.get('https://stat-display.herokuapp.com/player_stats.json').text)
 
 print('How many players are on the team?')
 data['player_count'] = int(raw_input())
 
-request_string = json.dumps(data)
+request_string = dumps(data)
 
-update_request = requests.post('https://stat-display.herokuapp.com/update-stats.php?key=6f070951-0da6-4349-ac6f-4b305875a6ab', data=request_string)
+update_request = post('https://stat-display.herokuapp.com/update-stats.php?key=6f070951-0da6-4349-ac6f-4b305875a6ab', data=request_string)
 print(update_request.status_code)
 print(update_request.text)
