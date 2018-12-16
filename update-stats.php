3a6d3f5b9187092bc4d3e9285cc0f5a2a1872263
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 18:54:27 -0800

    Update update-stats.php

--- update-stats.php
+++ update-stats.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   ];
   if (in_array($_GET['key'], $approved_keys)) {
     $request_body = file_get_contents('php://input');
-    $new_data = json_encode(utf8_decode(urldecode($request_body)));
+    $new_data = utf8_decode(urldecode($request_body));
     $data_file = fopen('player_stats.json', 'w') or die('Unable to open file');
     fwrite($data_file, $new_data) or die('Unable to write to file');
     fclose($data_file);
