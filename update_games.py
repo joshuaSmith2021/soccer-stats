df43d516f403878ac95cd934cffc9bce576f0193
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-15 19:02:17 -0800

    Update update_games.py

--- update_games.py
+++ update_games.py
@@ -1,2 +1,13 @@
 import json
 import requests
+
+data = json.loads(requests.get('https://stat-display.herokuapp.com/player_stats.json').text)
+
+print('How many games have been played?')
+data['games_played'] = raw_input()
+
+request_string = json.dumps(data)
+
+update_request = requests.post('https://stat-display.herokuapp.com/update-stats.php?key=6f070951-0da6-4349-ac6f-4b305875a6ab', data=request_string)
+print(update_request.status_code)
+print(update_request.text)
