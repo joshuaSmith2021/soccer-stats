23567aec66f2a98b0aa274d153c4e7123a1d5d91
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-17 17:07:08 -0800

    add info about which players are included

--- index.php
+++ index.php
@@ -4,23 +4,22 @@ $approved_keys = [
 ];
 if (in_array($_GET['key'], $approved_keys)) {
   echo '<!DOCTYPE html>
-  <html>
-  <head>
-    <title>JV Stats</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"/>
-    <link rel="stylesheet" href="https://smith2021.github.io/hermes/w3.css"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="main.css"/>
-  </head>
-  <body>
-    <div class="w3-padding roboto">
-      <h2>Stats By Player</h2>
-      <div id="statsContainer"></div>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
-    <script src="stat-display.js"></script>
-  </body>
-  </html>';
+<html>
+<head>
+  <title>JV Stats</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+  <link rel="stylesheet" href="https://smith2021.github.io/hermes/w3.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"/>
+</head>
+<body>
+  <div class="w3-padding roboto">
+    <h2>Stats By Player</h2>
+    <h6>Only players with at least one goals or one assist are shown.</h6>
+    <div id="statsContainer"></div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
+</body>
+</html>';
 } else {
   header('Location: https://stat-display.herokuapp.com/enter/?failed=true');
 }
