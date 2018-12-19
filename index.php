e2d872e2d85590e5a9e251a606966fc07c16df1c
soccerJoshNumberNine_terminal <32210099+joshuaSmith2021@users.noreply.github.com>
2018-12-19 15:14:24 -0800

    unlink minified js

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ if (in_array($_GET['key'], $approved_keys)) {
     </div>
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
-  <script src="stat-display.min.js"></script>
+  <script src="stat-display.js"></script>
 </body>
 </html>';
 } else {
